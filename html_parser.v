855d1b5b99f100d7538918292f729387ada7b5f2
ChrisEarman <cje4sw@virginia.edu>
2014-11-29 14:58:33 -0500

    stubbed out validPrefix and indexOfSubstring functions

--- html_parser.v
+++ html_parser.v
@@ -12,10 +12,32 @@ Print HelloWorld.
 
 Require Import Arith.
 
-Fixpoint getNameIndex (s: string) (index length: nat): nat :=
-  if (beq_nat index length)
-  then 0
-  else 1.
+Fixpoint ble_nat (n m : nat) : bool :=
+  match n with
+  | O => true
+  | S n' =>
+      match m with
+      | O => false
+      | S m' => ble_nat n' m'
+      end
+  end.
+
+Definition blt_nat (n m : nat) : bool :=
+  match ble_nat n m with
+    | true => negb (beq_nat n m)        
+    | false => false
+  end.
+
+
+Fixpoint validPrefix (prefix s : string): bool :=
+ true. 
+
+Fixpoint indexOfSubstring (sub s: string) (curIndex: nat): nat :=
+1.
+
+Definition getNameIndex (s: string): nat :=
+  let x := "itemprop=""name""" in
+  indexOfSubstring s x 0.
 
 Compute getNameIndex "sup" 1 1.
 Compute getNameIndex "sup" 1 3.
