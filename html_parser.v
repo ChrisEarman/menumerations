96bea95cd198b006aec599b23e2531a1686d235b
ChrisEarman <cje4sw@virginia.edu>
2014-11-29 21:04:42 -0500

    reorganized parser and began work on the ingredient section

--- html_parser.v
+++ html_parser.v
@@ -5,12 +5,9 @@ Require Import additional_tools.
 
 Local Open Scope string_scope.
 
-(* I initially planeed on making my own function for this
-   but then I realized that the function exists in the String library
-*)
-(* Fixpoint validPrefix (pre s : string): bool :=
- prefix pre s. *)
-
+(* -------------------------------
+   ------Basics-------------------
+   -------------------------------*)
 (* 
    This function takes in two strings, sub and s.
    it looks to see if sub is a substring of s,
@@ -31,6 +28,28 @@ Fixpoint indexOfSubstring (sub s: string): nat :=
        end.
 
 (*
+    This function takes in a string, s, and returns
+    the index of the first character a. The function
+    will return the length of the string s if there is 
+    no instance of the character a.
+*)
+Fixpoint getEndIndex (s: string) (a: ascii): nat := 
+  match s with
+    | EmptyString => 0
+    | String s1 s' => if (beq_ascii s1 a)
+                      then 0
+                      else S (getEndIndex s' "<")
+  end.
+
+(* ------ tests -------- *)
+Compute prefix "bob" "bobby". (*returns true*)
+Compute prefix "bobby" "bob". (*returns false*)
+
+(* -------------------------------
+   ------Recipe Name--------------
+   -------------------------------*)
+
+(*
    This function takes in a string, s, and returns
    the index of the name of the recipe. If s has no
    recognizable name, then the function will return 0.
@@ -43,21 +62,6 @@ Definition getNameIndex (s: string): nat :=
       else plus index 16 
 (*16 is the lenth of x, thus the name starts 16 spaces from the begining of x*).
 
-
-(*
-    This function takes in a string, s, and returns
-    the index of the first "<" character. The function
-    will return the length of the string s if there is 
-    no instance of the character "<".
-*)
-Fixpoint getEndIndex (s: string): nat := 
-  match s with
-    | EmptyString => 0
-    | String a s' => if (beq_ascii a "<")
-                     then 0
-                     else S (getEndIndex s')
-  end.
-
 (*
     This function takes in a string, s, and returns
     the length of the name in the string s.
@@ -65,7 +69,7 @@ Fixpoint getEndIndex (s: string): nat :=
 Definition getNameLength (s: string): nat := 
   let startIndex := getNameIndex s in
     let post := substring startIndex ((length s) - startIndex) s in 
-      getEndIndex post.
+      getEndIndex post "<".
 
 (*
     This function takes in a string, s, and returns
@@ -75,21 +79,8 @@ Definition getNameLength (s: string): nat :=
 Definition getName (s: string): string :=
   substring (getNameIndex s) (getNameLength s) s.
 
-
-
-
-
-
-
-(*
-   -------------
-   ----Tests----
-   -------------
-*)
+(* ------ tests -------- *)
 Compute getNameIndex "sup". (*Should return 0*)
-Compute prefix "bob" "bobby". (*returns true*)
-Compute prefix "bobby" "bob". (*returns false*)
-
 Example bChicken : string := "<h1 class=""fn"" itemprop=""name"">Bourbon Chicken</h1>".
 Example ssfChicken : string := 
         "<div class=""leady-hd rz-rec clrfix"">
@@ -102,3 +93,46 @@ Compute getNameIndex bChicken. (*should return 31*)
 Compute getNameLength bChicken. (*should return 15*)
 Compute getName bChicken. (*should return Bourbon Chicken*)
 Compute getName ssfChicken. (*should return Savory Southern Fried Chicken*)
+
+
+(* -------------------------------
+   ------Recipe Ingredients-------
+   -------------------------------*)
+
+Definition getIngredientIndex (s: string): nat:=
+  let x := "<span class=""ingredient"">" in
+    let index := indexOfSubstring x s in
+      if (beq_nat index (length s))
+      then 0
+      else plus index 25
+(*25 is the length of x, thus the ingredient starts 25 spaces from the begining of x*).
+
+Example ssfChicken2: string := 
+        "<li class=""ingredient"" itemprop=""ingredients"">
+            <span class=""ingredient"">
+                <span class=""amount"">
+                    <span class=""value"">2 </span>
+                    <span class=""type"">quarts</span>
+                </span>
+                <span class=""name"">
+                    <a href=""http://www.sitename.com/directory/subdirectory-12"">cold water</a>
+                </span>
+            </span>
+        </li>
+        <li class=""ingredient"" itemprop=""ingredients"">
+            <span =""ingredient"">
+                <span class=""amount"">
+                    <span class=""value"">2 </span>
+                    <span class=""type"">tablespoons</span>
+                </span>
+                <span class=""name"">
+                    <a href=""http://www.sitename.com/directory/subdirectory-33"">fine sea salt</a>
+                </span>
+            </span>
+        </li>".
+Compute getIngredientIndex ssfChicken2. (*should return 84*)
+
+
+
+
+
