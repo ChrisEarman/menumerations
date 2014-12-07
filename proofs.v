e4060d03f2fabbbeca40e2191cebc40cf6aa74f6
ChrisEarman <cje4sw@virginia.edu>
2014-12-07 17:41:45 -0500

    added one more proof, Theorem: noInstructionIndex.

--- proofs.v
+++ proofs.v
@@ -6,7 +6,6 @@ Require Import String.
           (indicating that no name was found) then
           getName should return the EmptyString.
 *)
-
 Theorem noNameIndex: 
         forall s: string, 
         getNameIndex s = 0 -> getName s = EmptyString.
@@ -18,3 +17,18 @@ Proof.
   reflexivity.
 Qed.
 
+(*
+   Proof: If the instructionIndex of any string returns 0 
+          (indicating that no instruction was found) then
+          getNextInstruction should return the EmptyString.
+*)
+Theorem noInstructionIndex:
+        forall s: string,
+        getInstructionIndex s = 0 -> getNextInstruction s = EmptyString.
+Proof.
+  intros.
+  unfold getNextInstruction.
+  rewrite -> H.
+  compute.
+  reflexivity.
+Qed.
