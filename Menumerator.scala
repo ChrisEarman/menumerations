9a60fb2dfb385a7e980856f9e6cc6d34e3b99b54
mazybear <zanizone45@gmail.com>
2014-12-08 22:50:04 -0500

    Implemented smarter ingredient association; fixed index out of bounds issue

--- Menumerator.scala
+++ Menumerator.scala
@@ -91,44 +91,74 @@ object Menumerator {
         return recipe.ingredients
     }
 
+    /**
+     *  Generates a new, randomized recipe by using an existing one as a
+     *  "seed", using the first few words of each of the "seed" recipe's 
+     *  instructions, and traversing the input graph to create new instructions.
+     *  Ingredients are associated with each word, and are used in the new
+     *  recipe if the associated word is a substring of the ingredient's text.
+     */
+    def getRandomizedRecipe(graph: FourGramGraph, collection: BSONCollection): Recipe = {
+        val seedRecipe = loadRandomRecipe(collection)
+        var tupleSet: Set[Tuple2[String, Int]] = Set()
+        var instructions: Seq[Instruction] = Seq()
+        var ingredients: Set[Ingredient] = Set()
+        for (instruction <- seedRecipe.instructions) {
+            val split = instruction.text.split("\\s+")
+            if (split.size >= 3) {
+                val res = graph.traverse(Tuple3(split(0).toLowerCase, split(1).toLowerCase, split(2).toLowerCase))
+                val generatedInstruction = split(0) + " " + split(1) + " " + split(2) + " " + res.unzip._1.mkString(" ")
+                tupleSet = tupleSet ++ res + Tuple2(split(0), seedRecipe.id) + 
+                    Tuple2(split(1), seedRecipe.id) + Tuple2(split(2), seedRecipe.id)
+                instructions = instructions :+ Instruction(generatedInstruction)
+            } else if (split.size == 2) {
+                tupleSet = tupleSet + Tuple2(split(0), seedRecipe.id) + 
+                    Tuple2(split(1), seedRecipe.id)
+                instructions = instructions :+ instruction
+            } else if (split.size == 1) {
+                tupleSet = tupleSet + Tuple2(split(0), seedRecipe.id)
+                instructions = instructions :+ instruction
+            }
+        }
+        for (tuple <- tupleSet) {
+            // TODO: Fix inefficiency with next command (should not have to look up same recipe >1 time)
+            val possibleIngs = getRecipeIngredients(tuple._2, collection)
+            for (ing <- possibleIngs) {
+                if (ing.text.toLowerCase.containsSlice(tuple._1.toLowerCase)) {
+                    ingredients = ingredients + ing
+                }
+            }
+        }
+        return Recipe(ingredients, instructions, 0)
+    }
+
     def run() = {
+        /**
+         *  Establishes connection to "recipe" collection in "menumerations" db
+         */
         val driver = new MongoDriver
         val connection = driver.connection(List("localhost"))
         val db = connection("menumerations")
         val recipeCollection = db("recipes")
+        
+        /**
+         *  Initialize graph and load all recipes
+         */
         val graph = new FourGramGraph
-        val res = loadAllRecipes(recipeCollection)
-        //TODO: Replace iterations with map function
-        for (recipe <- res) {
+        val allRecipes = loadAllRecipes(recipeCollection)
+
+        /**
+         *  Add all instructions across all recipes into the graph
+         */
+        // TODO: Replace iterations with map function
+        for (recipe <- allRecipes) {
             for (instruction <- recipe.instructions) {
                 graph.addSentence(instruction.text, recipe.id)
             }
         }
-        println("Graph created!")
-        graph.printGraph()
-        val seedRecipe = loadRandomRecipe(recipeCollection)
-        val seedInstructions = seedRecipe.instructions
-        var relevantRecipes: Set[Int] = Set()
-        var instructions: Seq[String] = Seq()
-        for (instruction <- seedInstructions) {
-            val split = instruction.text.split("\\s+")
-            if (split.size >= 3) {
-                val res = graph.traverse(Tuple3(split(0).toLowerCase, split(1).toLowerCase, split(2).toLowerCase)).unzip
-                val generatedInstruction = split(0) + " " + split(1) + " " + split(2) + " " + res._1.mkString(" ")
-                val relRecipes = res._2.toSet
-                relevantRecipes = relevantRecipes ++ relRecipes
-                instructions = instructions :+ generatedInstruction
-            } else {
-                instructions = instructions :+ instruction.text
-                relevantRecipes = relevantRecipes + seedRecipe.id
-            }
-        }
-        var ingredients: Set[Ingredient] = Set()
-        for (recipeId <- relevantRecipes) {
-            ingredients = ingredients ++ getRecipeIngredients(recipeId, recipeCollection)
-        }
-        println(ingredients)
-        println(instructions)
+
+        val randomRecipe = getRandomizedRecipe(graph, recipeCollection)
+        println(randomRecipe)
     }
 
     def main(args: Array[String]) {
